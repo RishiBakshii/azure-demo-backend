fa2e9e6da2b9d58ca78d02d3977969525029cdeb
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-09-07 02:13:49 +0530

    laetst ltate depl

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({"message":"running status 200"}),200
+@app.route("/about")
+def home():
+    return jsonify({"message":"about hello hellol"}),200
 
 if __name__=='__main__':
     app.run()
